170cd7d48f426228126459c4ea1e4ea36c82a631
Lorenzo Castillo <lorenzojcastillom@gmail.com>
2020-09-16 13:09:43 -0300

    test

--- src/App.js
+++ src/App.js
@@ -5,11 +5,8 @@ import MainPage from './views/mainPage.js';
 import Newsletter from './components/newsletter.js';
 import Footer from './components/footer.js';
 import ProductsGroup from './views/productsGroup.js';
-<<<<<<< HEAD
 import Contact from './views/contact.js';
-=======
 import ProductDetails from './views/productDetails';
->>>>>>> iñaki
 import injectContext from './store/appContext';
 
 function App() {
@@ -22,6 +19,7 @@ function App() {
         {/* This is supposed to receive a product id from database <:id> vvv */}
         <Route exact path="/products/id" component={ProductDetails} />
         {/* Aqui va la sección de Ceci */}
+        {/* Test */}
         <Route exact path="/contact" component={Contact} />
       </Switch>
       <Newsletter />
