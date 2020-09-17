296c67a39b7b44a23e9b71771cc2a5d7ea1167c0
ierrandonea <ierrandoneag@gmail.com>
2020-09-16 23:49:29 -0300

    added 'my account' section, incomplete

--- src/App.js
+++ src/App.js
@@ -5,12 +5,10 @@ import MainPage from './views/mainPage.js';
 import Newsletter from './components/newsletter.js';
 import Footer from './components/footer.js';
 import ProductsGroup from './views/productsGroup.js';
-<<<<<<< HEAD
 import Contact from './views/contact.js';
-=======
 import ProductDetails from './views/productDetails';
->>>>>>> iñaki
 import injectContext from './store/appContext';
+import MyAccount from './views/myAccount.js';
 
 function App() {
   return (
@@ -21,6 +19,8 @@ function App() {
         <Route exact path="/products" component={ProductsGroup} />
         {/* This is supposed to receive a product id from database <:id> vvv */}
         <Route exact path="/products/id" component={ProductDetails} />
+        {/* This is supposed to receive a username from database <:username> vvv */}
+        <Route exact path="/username" component={MyAccount} />
         {/* Aqui va la sección de Ceci */}
         <Route exact path="/contact" component={Contact} />
       </Switch>
