5a88c82e9bd58bf9becca721988d11c833041976
Lorenzo Castillo <lorenzojcastillom@gmail.com>
2020-09-21 18:05:39 -0300

    update 21/09

--- src/store/flux.js
+++ src/store/flux.js
@@ -1,9 +1,103 @@
 const getState = ({ getStore, getActions, setStore }) => {
     return {
         store: {
-            
+            apiURL: 'http://localhost:5000',
+            name: '',
+            last_name: '',
+            password: '',
+            email: '',
+            phone: '',
+            address: '',
+            currentUser: null,
+            error: null,
+            success: null            
         },
         actions: {
+            handleChangeLogin: e => {
+                setStore({
+                    [e.target.name]: e.target.value
+                })
+            },
+            register: async e => {
+                e.preventDefault();
+                const store = getStore();
+                const resp = await fetch(`${store.apiURL}/api/register`, {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        name: store.name,
+                        last_name: store.last_name,
+                        password: store.password,
+                        email: store.email,                        
+                        phone: store.phone,
+                        address: store.address
+                    }),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                })
+
+                const data = await resp.json();
+
+                const {msg} = data;
+
+                if (msg !== undefined) {
+                    setStore({
+                        error: msg
+                    })
+                } else {
+                    setStore({
+                        name: '',
+                        last_name: '',
+                        password: '',
+                        email: '',
+                        phone: '',
+                        address: '',
+                        currentUser: data,
+                        error: null                        
+                    })                   
+                    sessionStorage.setItem('currentUser', JSON.stringify(data))
+                }
+
+                console.log(data);
+            },
+            login: async e => {
+                e.preventDefault();
+                const store = getStore();
+                const resp = await fetch(`${store.apiURL}/api/login`, {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        email: store.email,                        
+                        password: store.password                                    
+                    }),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                })
+
+                const data = await resp.json();
+
+                const {msg} = data;
+
+                if (msg !== undefined) {
+                    setStore({
+                        error: msg
+                    })
+                } else {
+                    setStore({
+                        name: '',
+                        last_name: '',
+                        password: '',
+                        email: '',
+                        phone: '',
+                        address: '',
+                        currentUser: data,
+                        error: null                        
+                    })                    
+                    sessionStorage.setItem('currentUser', JSON.stringify(data))
+                }
+
+                console.log(data);
+            }
 
         }
     }
