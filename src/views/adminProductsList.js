bd3554bdfc2da372ce3b000630cd0a4186193e8f
ierrandonea <ierrandoneag@gmail.com>
2020-10-21 11:46:34 -0300

    updated readme file

--- src/views/adminProductsList.js
+++ src/views/adminProductsList.js
@@ -1,10 +1,9 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Context } from '../store/appContext';
 
-const AdminProductsList = props => {
+const AdminProductsList = (tabs, setTabs) => {
     const { store, actions } = useContext(Context);
-    const location = useLocation();
 
     const [brewing, setBrewing] = useState({
         sorting: "priceup",
@@ -15,14 +14,12 @@ const AdminProductsList = props => {
     });
 
     useEffect(() => {
-        // fetchs products based on sorting order        
         actions.getProductsFiltered(brewing)
-        console.log(brewing)
     }, [brewing]);
 
 
-    // sets apiURL from onChange in <select id="sortCombo"> 
-    const handleSort = (e) => {
+    // sets apiURL from onChange in <select id="usersSortCombo"> 
+    const handleSortProducts = e => {
         if (e.target.value == 2) {
             setBrewing({ ...brewing, sorting: "pricedown" });
         }
@@ -38,59 +35,56 @@ const AdminProductsList = props => {
     };
 
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-12">
-                    <h2 className="text-center mt-5">Product List Administration</h2>
-                    {/* Table div */}
-                    <div className="d-flex justify-content-end align-items-baseline mt-n2 mb-n3">
-                        <p className="mr-2">Ordenar por</p>
-                        <select className="custom-select w-25" id="usersSortCombo" onChange={e => handleSort(e)}>
-                            <option value="1">Precio (Ascendente)</option>
-                            <option value="2">Precio (Descendente)</option>
-                            <option value="3">Marca (A...Z)</option>
-                            <option value="4">Marca (Z...A)</option>
-                        </select>
-                    </div>
-                    <div className="table-responsive mt-5">
-                        <table className="table table-striped text-center">
-                            <thead className="thead-dark">
+        <div className="container-fluid">
+            <div className="d-flex justify-content-end align-items-baseline my-3">
+                <p className="mr-2">Ordenar por</p>
+                <select className="custom-select w-25" id="usersSortCombo" onChange={e => handleSortProducts(e)}>
+                    <option value="1">Precio (Ascendente)</option>
+                    <option value="2">Precio (Descendente)</option>
+                    <option value="3">Marca (A...Z)</option>
+                    <option value="4">Marca (Z...A)</option>
+                </select>
+            </div>
+            <table className="table table-striped text-center">
+                <thead className="c-coffee text-white">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Producto</th>
+                        <th scope="col">Precio</th>
+                        <th scope="col">Cantidad</th>
+                        <th scope="col">Marca</th>
+                        <th scope="col">Sku</th>
+                        <th scope="col">Presentación</th>
+                        <th scope="col">Imagen</th>
+                        <th scope="col">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {!!store.products &&
+                        store.products.map((product, index) => {
+                            return (
                                 <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">Product</th>
-                                    <th scope="col">Price</th>
-                                    <th scope="col">Stock</th>
-                                    <th scope="col">Brand</th>
-                                    <th scope="col">sku</th>
-                                    <th scope="col">Presentation</th>
-                                    <th scope="col">Image</th>
-                                    <th></th>
-                                    <th></th>
+                                    <th scope="row">{index + 1}</th>
+                                    <td>{product.name}</td>
+                                    <td>{(product.price).toLocaleString('en-US', { style: 'currency', currency: 'CLP', }) /* $2,500.00 */}</td>
+                                    <td>{product.stock}</td>
+                                    <td>{product.brand}</td>
+                                    <td>{product.sku}</td>
+                                    <td>{product.ground}<br />{product.presentation}</td>
+                                    <td><img src={process.env.REACT_APP_URL_API + "products/coffee/" + product.image} alt="" className="border border-2 border-dark thumbnail" /></td>
+                                    <td className="d-flex align-items-baseline">
+                                        <Link to={`/admincoffee/editproduct/${product.id}`} className="btn btn-sm btn-dark ml-5" onClick={() => actions.setCurrentProduct(product)}>Editar</Link>
+                                        <button className="btn btn-sm btn-danger ml-3" data-toggle="modal" data-target="#deleteProductModal" onClick={() => { setTabs({ ...tabs, productID: product.id, index: index }) }}>Eliminar</button>
+                                    </td>
                                 </tr>
-                            </thead>
-                            <tbody>
-                                {!!store.products &&
-                                    store.products.map((product, index) => {
-                                        return (
-                                            <tr>
-                                                <th scope="row">{index + 1}</th>
-                                                <td>{product.name}</td>
-                                                <td>{(product.price).toLocaleString('en-US', { style: 'currency', currency: 'CLP', }) /* $2,500.00 */}</td>
-                                                <td>{product.stock}</td>
-                                                <td>{product.brand}</td>
-                                                <td>{product.sku}</td>
-                                                <td>{product.presentation}</td>
-                                                <td>{product.image}</td>
-                                                <td><Link to="/admincoffee/editproduct/" className="btn btn-sm btn-dark mr-3" onClick={()=>actions.setCurrentProduct(product)}>Editar</Link></td>
-                                                <td><button className="btn btn-sm btn-danger ml-3" onClick={()=>actions.deleteProducts(product.id, index)}>Eliminar</button></td>                                                  
-                                            </tr>
-                                        )
-                                    })
-                                }
-                            </tbody>
-                        </table>                        
-                    </div>                    
-                </div>
+                            )
+                        })
+                    }
+                </tbody>
+            </table>
+            <hr />
+            <div className="d-flex justify-content-end">
+                <Link to="/admincoffee/addProduct"><button className="btn c-coffee text-white mr-2">Crear Nuevo</button></Link>
             </div>
         </div>
     )
